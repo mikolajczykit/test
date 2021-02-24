cf2064803046ce7a5feeaa47d210b15bd6a86f9c
Marek Mikolajczyk <mikolajczyk.it@gmail.com>
2021-02-24 16:28:47 +0100

    init commit

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace repotest
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
